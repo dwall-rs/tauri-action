a276df81b2d779ecf75501e587244236f52958ad
thepoy <thepoy@163.com>
2025-04-17 12:29:21 +0800

    refactor: standardize import statements and improve code formatting
    - Updated import statements to use double quotes consistently across files.
    - Reformatted code for better readability by aligning parameters and arguments.
    - Simplified the `execTauriCommand` method in the `Runner` class by removing unnecessary parameters.
    - Enhanced the `getRunner` function to handle script commands more effectively and ensure compatibility with different package managers.

--- src/runner.ts
+++ src/runner.ts
@@ -1,93 +1,90 @@
-import { TauriConfig } from './config';
+import { TauriConfig } from "./config";
 import {
-  execCommand,
-  getTauriDir,
-  hasDependency,
-  retry,
-  usesBun,
-  usesPnpm,
-  usesYarn,
-} from './utils';
+	execCommand,
+	getTauriDir,
+	hasDependency,
+	retry,
+	usesBun,
+	usesPnpm,
+	usesYarn,
+} from "./utils";
 
 class Runner {
-  // Could be "npm", "yarn", "pnpm", "bun", "cargo", "path/to/tauri-cli/binary" or "tauri"
-  bin: string;
-  // could be ["tauri"], ["run", "tauri"], ["some package.json script"], ["run", "some package.json script"] or []
-  tauriScript: string[];
+	// Could be "npm", "yarn", "pnpm", "bun", "cargo", "path/to/tauri-cli/binary" or "tauri"
+	bin: string;
+	// could be ["tauri"], ["run", "tauri"], ["some package.json script"], ["run", "some package.json script"] or []
+	tauriScript: string[];
 
-  constructor(bin: string, tauriScript?: string[]) {
-    this.bin = bin;
-    this.tauriScript = tauriScript || [];
-  }
+	constructor(bin: string, tauriScript?: string[]) {
+		this.bin = bin;
+		this.tauriScript = tauriScript || [];
+	}
 
-  async execTauriCommand(
-    command: string[],
-    commandOptions: string[],
-    cwd?: string,
-    env?: Record<string, string>,
-    retryAttempts: number = 0,
-  ): Promise<void> {
-    const args: string[] = [];
+	async execTauriCommand(
+		commandOptions: string[],
+		cwd?: string,
+		env?: Record<string, string>,
+		retryAttempts = 0,
+	): Promise<void> {
+		const args: string[] = [];
 
-    if (this.bin === 'npm' && this.tauriScript[0] !== 'run') {
-      args.push('run');
-    }
+		if (this.bin === "npm" && this.tauriScript[0] !== "run") {
+			args.push("run");
+		}
 
-    args.push(...this.tauriScript);
+		args.push(...this.tauriScript);
 
-    args.push(...command);
+		if (this.bin === "npm" && commandOptions.length) {
+			args.push("--");
+		}
 
-    if (this.bin === 'npm' && commandOptions.length) {
-      args.push('--');
-    }
+		args.push(...commandOptions);
 
-    args.push(...commandOptions);
-
-    return retry(
-      () => execCommand(this.bin, args, { cwd }, env),
-      retryAttempts + 1,
-    ) as Promise<void>;
-  }
+		return retry(
+			() => execCommand(this.bin, args, { cwd }, env),
+			retryAttempts + 1,
+		) as Promise<void>;
+	}
 }
 
 async function getRunner(
-  root: string,
-  tauriScript: string | null,
+	root: string,
+	tauriScript: string | null,
 ): Promise<Runner> {
-  if (tauriScript) {
-    // FIXME: This will also split file paths with spaces.
-    const [runnerCommand, ...runnerArgs] = tauriScript.split(' ');
-    return new Runner(runnerCommand, runnerArgs);
-  }
+	if (tauriScript) {
+		// FIXME: This will also split file paths with spaces.
+		const [runnerCommand, ...runnerArgs] = tauriScript.split(" ");
+		return new Runner(runnerCommand, runnerArgs);
+	}
 
-  if (hasDependency('@tauri-apps/cli', root)) {
-    if (usesYarn(root)) return new Runner('yarn', ['tauri']);
-    if (usesPnpm(root)) return new Runner('pnpm', ['tauri']);
-    if (usesBun(root)) return new Runner('bun', ['tauri']);
-    return new Runner('npm', ['run', 'tauri']);
-  }
+	if (hasDependency("@tauri-apps/cli", root)) {
+		if (usesYarn(root)) return new Runner("yarn", ["tauri", "build"]);
+		if (usesPnpm(root)) return new Runner("pnpm", ["tauri", "build"]);
+		if (usesBun(root)) return new Runner("bun", ["tauri", "build"]);
+		return new Runner("npm", ["run", "tauri", "build"]);
+	}
 
-  // TODO: Change to v2 after a while.
-  let tag = 'v1';
+	// TODO: Change to v2 after a while.
+	let tag = "v1";
 
-  try {
-    const tauriDir = getTauriDir(root);
-    if (tauriDir) {
-      const baseConf = TauriConfig.fromBaseConfig(tauriDir);
+	try {
+		const tauriDir = getTauriDir(root);
+		if (tauriDir) {
+			const baseConf = TauriConfig.fromBaseConfig(tauriDir);
 
-      if (baseConf && baseConf.isV2()) {
-        tag = 'v2';
-      }
-    }
-  } catch {
-    // ignore
-  }
+			if (baseConf?.isV2()) {
+				tag = "v2";
+			}
+		}
+	} catch {
+		// ignore
+	}
 
-  await execCommand('npm', ['install', '-g', `@tauri-apps/cli@${tag}`], {
-    cwd: undefined,
-  });
+	await execCommand("npm", ["install", "-g", `@tauri-apps/cli@${tag}`], {
+		cwd: undefined,
+	});
 
-  return new Runner('tauri');
+	return new Runner("tauri", ["build"]);
 }
 
 export { Runner, getRunner };
